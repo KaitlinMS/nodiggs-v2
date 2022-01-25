e1fe035cc292364a78e77a74bac57e7c1f8ea014
Kaitlin <smith.kaitlin@gmail.com>
2022-01-25 01:01:07 -0500

    Removes tutorial files.

--- commands/shield.js
+++ commands/shield.js
@@ -1,12 +1,12 @@
-// TODO: set up the shield command
-
 const { SlashCommandBuilder } = require('@discordjs/builders');
+
+// TODO: set up the shield command
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('shield')
         .setDescription('Hide bad stuff.'),
     async execute(interaction) {
-        await interaction.reply('Shield goes here!');
+        await interaction.reply('Avert your eyes!!');
     },
 };
