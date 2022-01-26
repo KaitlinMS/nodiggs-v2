db9f43e3992f951af1b9a10d305f0d605c89f26e
Kaitlin <smith.kaitlin@gmail.com>
2022-01-25 22:48:21 -0500

    Completes the Shield command.

--- commands/shield.js
+++ commands/shield.js
@@ -1,12 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-// TODO: set up the shield command
+let shield = "```\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n " +
+    "   |\\                     /) \n" +
+    " /\\_\\\\__               (_//  \n" +
+    "|   `>\\-`     _._       //`) \n" +
+    " \\ /` \\\\  _.-`:::`-._  //    \n" +
+    "  `    \\|`    :::    `|/     \n" +
+    "        |     :::     |      \n" +
+    "        |.....:::.....|      \n" +
+    "        |S H I E L D !|      \n" +
+    "        |     :::     |      \n" +
+    "        \\     :::     /      \n" +
+    "         \\    :::    /       \n" +
+    "          `-. ::: .-'        \n" +
+    "           //`:::`\\\\         \n" +
+    "          //   '   \\\\        \n" +
+    "         |/         \\\\       \n" +
+    "\n \n \n \n \n \n \n \n \n ```";
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('shield')
         .setDescription('Hide bad stuff.'),
     async execute(interaction) {
-        await interaction.reply('Avert your eyes!!');
+        await interaction.reply(shield);
     },
 };
