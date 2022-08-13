ba66efc8e12f876206c89785ea883977b30a6b64
Kaitlin <smith.kaitlin@gmail.com>
2022-08-13 09:55:34 -0400

    Small amount of cleanup.

--- bot.js
+++ bot.js
@@ -4,21 +4,27 @@ const { token } = require('./config.json');
 
 // Create a new client instance
 // Intents give the bot permissions to perform certain actions, or tell it to listen for certain events
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILDS] });
+const client = new Client(
+    { intents: [ Intents.FLAGS.GUILDS,
+                Intents.FLAGS.GUILD_MESSAGES,
+                Intents.FLAGS.GUILD_MEMBERS,
+                Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+                Intents.FLAGS.GUILDS ]
+    });
 
-// Set up all the commands
+// Iterate through all commands and set them up
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
 
-    // Set a new item in the Collection
+    // Create a new item in the Collection
     // With the key as the command name and the value as the exported module
     client.commands.set(command.data.name, command);
 }
 
-// Set up all the events and react to them
+// Set up all the events and get ready to react to them
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
@@ -30,7 +36,7 @@ for (const file of eventFiles) {
     }
 }
 
-// React to a slash command that comes in
+// Wait for slash commands and react to them when they come in
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
